5facf16f45f72726d0cb7d03957a1e560b122be2
Bartosz Sobczak <bartoszsobczak95@gmail.com>
2016-05-30 13:35:24 +0200

    Copy LRU algorithm from SO_Lab3 project

--- src/Algorithms.java
+++ src/Algorithms.java
@@ -26,4 +26,40 @@ public class Algorithms {
 		
 		return pages_errors;
 	}
+	
+	public static int LRU(int frame_number, ArrayList<Integer> input_references) {
+		int page_errors = 0;
+		int remove_index = 0; 
+		ArrayList<Integer> frames_used = new ArrayList<>(); 	// indexes of frames which should be replaced (descending order)
+		int[] frames;
+		
+		ArrayList<Integer> references = (ArrayList<Integer>) input_references.clone();
+		
+		if(frame_number < references.size()){
+			frames = new int[frame_number];
+			for(int i = 0; i < frame_number; i++){
+				frames[i] = i;
+				frames_used.add(frames[i]);
+			}	
+			//fill frames at the beginning					
+					
+			while(!references.isEmpty()){
+				int index = 0;
+				while(index < frame_number && frames[index] != references.get(0))
+					index++;
+				if(index < frame_number){
+					int tmp = frames_used.remove(frames_used.indexOf(index));	//delete index of current reference in frames_used and add it to the end of the list
+					frames_used.add(tmp);														
+					references.remove(0);
+				}else{
+					remove_index = frames_used.remove(0);
+					frames_used.add(remove_index);
+					frames[remove_index] = references.remove(0);
+
+					page_errors++;
+				}
+			}
+		}	
+			return page_errors;
+	} // LRU algorithm
 }
